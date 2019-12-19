0c6e0a4289f661fc1b2e5ab6a940e96db0b07c99
RukiyeSahin <58738747+RukiyeSahin@users.noreply.github.com>
2019-12-19 13:48:45 +0300

    Add files via upload

---
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{ //Product Class
+    class Araba
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public double KM { get; set; }
+        public bool Vites { get; set; }
+        public override string ToString()
+        {
+            return $"{Marka} marka araba {Model} modelinde {KM} kilometrede {Vites} vites olarak üretilmiştir.";
+        }
+    }
+    //Builder
+    abstract class ArabaBuilder
+    {
+        protected Araba araba;
+        public Araba Araba
+        {
+            get { return araba; }
+        }
+
+        public abstract void SetMarka();
+        public abstract void SetModel();
+        public abstract void SetKM();
+        public abstract void SetVites();
+    }
+    //ConcreteBuilder Class
+    class OpelConcreteBuilder : ArabaBuilder
+    {
+        public OpelConcreteBuilder()
+        {
+            araba = new Araba();
+        }
+        public override void SetKM() => araba.KM = 100;
+        public override void SetMarka() => araba.Marka = "Opel";
+        public override void SetModel() => araba.Model = "Astra";
+        public override void SetVites() => araba.Vites = true;
+    }
+    //ConcreteBuilder Class
+    class ToyotaConcreteBuilder : ArabaBuilder
+    {
+        public ToyotaConcreteBuilder()
+        {
+            araba = new Araba();
+        }
+        public override void SetKM() => araba.KM = 150;
+        public override void SetMarka() => araba.Marka = "Toyota";
+        public override void SetModel() => araba.Model = "Corolla";
+        public override void SetVites() => araba.Vites = true;
+    }
+    //ConcreteBuilder Class
+    class BMWConcreteBuilder : ArabaBuilder
+    {
+        public BMWConcreteBuilder()
+        {
+            araba = new Araba();
+        }
+        public override void SetKM() => araba.KM = 25;
+        public override void SetMarka() => araba.Marka = "BMW";
+        public override void SetModel() => araba.Model = "X5";
+        public override void SetVites() => araba.Vites = true;
+    }
+    //Director Class
+    class ArabaUret
+    {
+        public void Uret(ArabaBuilder Araba)
+        {
+            Araba.SetKM();
+            Araba.SetMarka();
+            Araba.SetModel();
+            Araba.SetVites();
+        }
+    }
+    //Client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArabaBuilder araba = new OpelConcreteBuilder();
+            ArabaUret uret = new ArabaUret();
+            uret.Uret(araba);
+
+            Console.WriteLine(araba.Araba.ToString());
+
+            araba = new ToyotaConcreteBuilder();
+            uret.Uret(araba);
+            Console.WriteLine(araba.Araba.ToString());
+
+            Console.Read();
+        }
+    }
+    
+}
